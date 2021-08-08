501ed442117b234a9b346fa49a59831c4c263c51
ValeriiHub <p04taera@gmail.com>
2021-08-08 17:51:02 +0300

    Initial Commit

---
+++ RealmApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealmApp
+//
+//  Created by Valerii D on 08.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
